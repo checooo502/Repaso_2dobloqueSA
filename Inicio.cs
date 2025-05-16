a77c746dd32e16bb7d51957945f178f4f245608b
5to Bachillerato A <5to Bachillerato A@DESKTOP-MR3I3QR>
2025-05-16 10:37:38 -0600

    Agregar archivos de proyecto.

---
+++ Inicio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Repaso_2dobloqueSA
+{
+    public partial class frmInicio : Form
+    {
+        public frmInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void frmInicio_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NumAleatorio repaso1 = new NumAleatorio();
+            repaso1.Show();
+             
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AlumnoAleatorio Repaso2 = new AlumnoAleatorio();
+            Repaso2.Show();
+        }
+    }
+}
---
+++ NumAleatorio.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Repaso_2dobloqueSA
+{
+    public partial class NumAleatorio : Form
+    {
+        int aleatorio = 0;
+        Random rand = new Random();
+        public NumAleatorio()
+        {
+            InitializeComponent();
+        }
+
+        private void NumAleatorio_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNumMax_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(Button.Text, out int numMax))
+            {
+                aleatorio = rand.Next(1, numMax);
+                MessageBox.Show($"numero; {aleatorio}");
+            }
+            else
+            {
+                MessageBox.Show("ingrese un numero valido");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
